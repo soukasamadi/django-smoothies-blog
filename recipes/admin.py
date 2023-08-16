17a602d08cda1f98ef0c1e6ffa383928666a04a1
soukasamadi <>
2023-08-16 23:09:01 +0000

    Create superuser and admin model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Category, Author, Recipe
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Add fields for Category in admin panel
+    """
+    list_display = ['title']
+    search_fields = ['title']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Add fields for Author in admin panel
+    """
+    list_display = ('author', 'timestamp')
+    search_fields = ['author']
+
+
+@admin.register(Recipe)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Add fields which will use summernote editor in admin panel
+    """
+    list_display = ('title', 'slug', 'status', 'timestamp', 'author',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'timestamp')
+    summernote_fields = ('content')
