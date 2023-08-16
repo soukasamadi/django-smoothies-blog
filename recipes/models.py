4e372b33548ec3b1b2815251d4b977897637e114
soukasamadi <>
2023-08-16 23:32:30 +0000

    Apply migrations and change default and maxValueValidator

--- recipes/models.py
+++ recipes/models.py
@@ -48,10 +48,10 @@ class Recipe(models.Model):
     time_to_prepar = models.IntegerField(default=0)
     featured_image = CloudinaryField('image', default='placeholder')
     stars = models.PositiveIntegerField(
-        default=1,
+        default=3,
         validators=[
             MinValueValidator(1),
-            MaxValueValidator(8)])
+            MaxValueValidator(5)])
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
